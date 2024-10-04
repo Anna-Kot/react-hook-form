3298389f355a0e14dac5c93b59285ce5432fbf38
Anna Kotvitska <annakotvitska123@gmail.com>
2024-10-04 20:43:00 +0300

    created general form (with react hook form)

---
+++ src/components/YouTudeForm.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
+
+let renderCount = 0;
+
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
+const YouTudeForm = () => {
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
+  //   const { name, ref, onChange, onBlur } = register('username');
+
+  const onSubmit = (data: FormValues) => {
+    console.log('submitred form: ', data);
+  };
+
+  renderCount++;
+  return (
+    <div>
+      <h1>YouTube Form ({renderCount / 2})</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor='username'>Username</label>
+        <input type='text' id='username' {...register('username')} />
+        {/* <input
+          type='text'
+          id='username'
+          name={name}
+          ref={ref}
+          onChange={onChange}
+          onBlur={onBlur}
+        /> */}
+
+        <label htmlFor='email'>E-mail</label>
+        <input type='email' id='email' {...register('email')} />
+
+        <label htmlFor='channel'>Channel</label>
+        <input type='text' id='channel' {...register('channel')} />
+
+        <button>Submit</button>
+      </form>
+      <DevTool control={control} />
+    </div>
+  );
+};
+
+export default YouTudeForm;
